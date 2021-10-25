0787b39ed620e6dfe43fad22c8183fc1ba62fc52
Kaytlyn Guerrero <kaytlynguerrero@x-10-5-37-148.wifi.stedwards.edu>
2021-10-25 16:28:07 -0500

    KG initial commit

---
+++ Profitable_Choice/src/main/java/com/steds/ProfitableChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.steds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitableChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitableChoiceApplication.class, args);
+	}
+
+}

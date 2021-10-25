0f96f35da7f97602f889ec856625c67e0e4e04eb
Sabur Khan <saburkhanatx@gmail.com>
2021-10-25 17:53:26 -0500

    intial commit

---
+++ Profitable_Choice/src/main/java/com/steds/controller/TickerController.java
@@ -0,0 +1,4 @@
+package com.steds.controller;
+
+public class TickerController {
+}
---
+++ Profitable_Choice/src/main/java/com/steds/model/Ticker.java
@@ -0,0 +1,11 @@
+package com.steds.model;
+
+public class Ticker {
+    private String symbol;
+    private String name;
+    private String currency;
+    private String stockExchange;
+    private String exchangeShortName;
+
+
+}

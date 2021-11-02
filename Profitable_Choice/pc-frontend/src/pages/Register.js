f25e8c3554b260c71b4f92b290949107b35be840
Kaytlyn Guerrero <kaytlynguerrero@Kaytlyns-MacBook-Air.local>
2021-11-02 11:16:52 -0500

    KG trying to add register

--- Profitable_Choice/pc-frontend/src/pages/Register.js
+++ Profitable_Choice/pc-frontend/src/pages/Register.js
@@ -1,7 +1,48 @@
-function RegisterPage() {
-    return <div>
-        Registration Page
-    </div>
-
+import axios from "axios";
+import React, { Component } from "react";
+export default class RegisterUser extends React.Component {
+    state = {
+        userName: ''
+        // firstName: '',
+        // lastName: '',
+        // email: '',
+        // password: ''
+    }
+    handleChange = event => {
+        this.setState({userName: event.target.value,
+                        // firstName: event.target.value,
+                        // lastName: event.target.value,
+                        // email: event.target.value })
+        })
+    }
+    handleSubmit = event => { 
+        event.preventDefault();
+        const user = {
+            userName: this.state.userName
+            // firstName: this.state.firstName,
+            // lastname: this.state.lastName,
+            // email: this.state.email,
+            // password: this.state.password
+        }
+        axios.post("http://localhost:8080/register", { user })
+        .then( res => {
+            console.log(res)
+            console.log(res.data)
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Username: 
+                        <input type="text"
+                            userName="userName"
+                            onChange={this.handleSubmit} />
+                    </label>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        )
+    }
 }
-export default RegisterPage;

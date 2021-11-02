3858d3a1b1eb8e70c942c98cbed622213b66ab3f
Sabur Khan <saburkhanatx@gmail.com>
2021-11-01 20:16:28 -0500

    Fetching Data from set variable... gone right

--- Profitable_Choice/pc-frontend/src/pages/TickerSearch.js
+++ Profitable_Choice/pc-frontend/src/pages/TickerSearch.js
@@ -4,52 +4,55 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios"
 import reportWebVitals from "../reportWebVitals";
+import { Link } from 'react-router-dom';
  
 
 class stockProfile extends React.Component {
 
     constructor(props){
         super(props)
-        this.state = {companyProfile:""}
+        this.state = {companyProfile: {} };
         
     }
     
     handleChange = (e) => {
         const {id, value} = e.target
-        this.state[id] = value
+       // this.state[id] = value
+        this.setState({[id]:value})
+        
     }
-
+    
     handleSubmit = (e) => {
         /* This prevents the page from being refreshed when submitting the input */
     e.preventDefault();
     fetch("http://localhost:8080/search_ticker/"+this.state.ticker)
     .then(response => response.json())
-    .then(data => this.state.companyProfile=data)
-    console.log(this.state);
+    .then(data => this.setState({companyProfile:data}), () => console.log(this.state))
+   // console.log(this.state);
     };
+    
 
-
-    render() {
-        return (
-            <div className = "create">
-                <h2> Input Ticker! </h2>
-                <form onSubmit= {this.handleSubmit}>
-                    <label>Ticker: </label>
-                    <input
-                    id = "ticker"
-                    type = "text"
-                    maxLength={4}
-                    required
-                /* We are creating a function that is taking an event object and targeting the title value */
-                    onChange = {this.handleChange}
-                    />
-                    <button> Submit </button>
-                    {/* Printing the tracked input from the input box */}
-                    <p>
-                    </p>
-                </form>
-            </div>
-        )
+    render() { 
+            return(
+                <div className = "create">
+                    <h2> Input Ticker! </h2>
+                    <form onSubmit= {this.handleSubmit}>
+                        <label>Ticker: </label>
+                        <input
+                        id = "ticker"
+                        type = "text"
+                        maxLength={4}
+                        required
+                    /* We are creating a function that is taking an event object and targeting the title value */
+                        onChange = {this.handleChange}
+                        />
+                        <button> Submit </button>
+                    </form>
+                <h2>
+                    {this.state.companyProfile.companyName}
+                </h2>
+                </div>
+            )   
       }
 }
 export default stockProfile;

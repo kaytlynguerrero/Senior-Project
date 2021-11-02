242ffcc2ac48cfc9090db587a6f4eb253ca981df
Sabur Khan <saburkhanatx@gmail.com>
2021-11-01 23:36:44 -0500

    register endpoint and UserForm model class update

--- Profitable_Choice/src/main/java/com/steds/model/UserForm.java
+++ Profitable_Choice/src/main/java/com/steds/model/UserForm.java
@@ -1,11 +1,10 @@
 package com.steds.model;
 
 public class UserForm {
-    private Long userId;
+
     private String userName;
     private String firstName;
     private String lastName;
-    private boolean enabled;
     private String gender;
     private String email;
     private String password;
@@ -14,24 +13,15 @@ public class UserForm {
     public UserForm(){
 
     }
-    public UserForm(Long userId, String userName, String firstName, String lastName, boolean enabled, String gender, String email, String password, String confirmPassword) {
-        this.userId = userId;
+    public UserForm(String userName, String firstName, String lastName, String gender, String email, String password) {
+
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.enabled = enabled;
         this.email = email;
         this.password = password;
-        this.confirmPassword = confirmPassword;
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 
     public String getUserName() {
         return userName;
@@ -55,14 +45,6 @@ public class UserForm {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 
     public String getGender() {

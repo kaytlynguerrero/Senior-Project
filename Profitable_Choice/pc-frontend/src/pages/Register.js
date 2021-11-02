84b14c9086dd2f6528af511b8b5f8d3c1283b8ec
Sabur Khan <saburkhanatx@gmail.com>
2021-11-02 14:53:47 -0500

    fe update

--- Profitable_Choice/pc-frontend/src/pages/Register.js
+++ Profitable_Choice/pc-frontend/src/pages/Register.js
@@ -2,28 +2,37 @@ import axios from "axios";
 import React, { Component } from "react";
 export default class Register extends React.Component {
     state = {
-        username: ''
-        // firstName: '',
-        // lastName: '',
-        // email: '',
-        // password: ''
+        userName: '',
+        firstName: '',
+        lastName: '',
+        gender: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+
     }
     handleChange = event => {
-        this.setState({username: event.target.value
+        const {id, value} = event.target
+        this.setState({[id]:value})
+       // this.setState({userName: event.target.value
                         // firstName: event.target.value,
                         // lastName: event.target.value,
                         // email: event.target.value })
-        })
+       // })
     }
     handleSubmit = event => { 
         event.preventDefault();
         const user = {
-            username: this.state.username
-            // firstName: this.state.firstName,
-            // lastname: this.state.lastName,
-            // email: this.state.email,
-            // password: this.state.password
+            userName: this.state.userName,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            gender: this.state.gender,
+            email: this.state.email,
+            password: this.state.password,
+            confirmPassword: this.state.confirmPassword,
         }
+        console.log({user});
+        //console.log(this.state);
         axios.post("http://localhost:8080/register", { user })
         .then( res => {
             console.log(res)
@@ -37,8 +46,62 @@ export default class Register extends React.Component {
                     <label>
                         Username: 
                         <input type="text"
-                            username="userName"
+                            id ="userName"
                             onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        First Name:
+                        <input type ="text"
+                            id = "firstName"
+                            type = "text"
+                            required
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        Last Name:
+                        <input type ="text"
+                            id = "lastName"
+                            type = "text"
+                            required
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        Gender:
+                        <input type ="text"
+                            id = "gender"
+                            type = "text"
+                            required
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input type ="text"
+                            id = "email"
+                            type = "text"
+                            required
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input type ="text"
+                            id = "password"
+                            type = "text"
+                            required
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        Confirm Password:
+                        <input type ="text"
+                            id = "confirmPassword"
+                            type = "text"
+                            required
+                            onChange={this.handleChange}
+                        />
                     </label>
                     <button type="submit">Register</button>
                 </form>

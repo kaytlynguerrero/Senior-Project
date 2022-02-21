8c6e5f4e1be3e99f01c35b69da55b2a5a2e3bb36
Sabur Khan <saburkhanatx@gmail.com>
2022-02-21 09:54:39 -0600

    stock news

--- Profitable_Choice/pc-frontend/src/components/pages/GraphResult.js
+++ Profitable_Choice/pc-frontend/src/components/pages/GraphResult.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import './GraphResult.css';
+import CardItem from '../CardItem';
 
 class GraphResult extends React.Component {
   constructor(props) {
@@ -336,7 +337,66 @@ handleNewCompanySearchSubmit = (e) => {
 
           </table>
         </div>
+    
+        
+        <div className='cards'>
+       
+        <div className='cards__container'>
+  
+          <div className='cards__wrapper'>
+  
+            <ul className='cards__items'>
+            {/* <Link to ={{pathname: "https://www.marketwatch.com/markets" }} target="_blank" /> */}
+              <CardItem 
+                src={this.state.stockNews[0].image}
+                text={this.state.stockNews[0].text}
+                label={this.state.stockNews[0].title}
+                path={this.state.stockNews[0].url}
+              />
+               <CardItem 
+                
+                src={this.state.stockNews[5].image}
+                text={this.state.stockNews[5].text}      
+                label={this.state.stockNews[5].title} 
+                path={this.state.stockNews[5].url} 
+              />
+              <CardItem 
+      
+                src={this.state.stockNews[1].image}
+                text={this.state.stockNews[1].text}      
+                label={this.state.stockNews[1].title} 
+                path={this.state.stockNews[1].url} 
+              />
+            </ul>
+            <ul className='cards__items'>
+              <CardItem
+               src={this.state.stockNews[2].image}
+               text={this.state.stockNews[2].text}      
+               label={this.state.stockNews[2].title} 
+               path={this.state.stockNews[2].url} 
+              />
+              <CardItem
+                src={this.state.stockNews[3].image}
+                text={this.state.stockNews[3].text}      
+                label={this.state.stockNews[3].title} 
+                path={this.state.stockNews[3].url} 
+              />
+              <CardItem
+                src={this.state.stockNews[4].image}
+                text={this.state.stockNews[4].text}      
+                label={this.state.stockNews[4].title} 
+                path={this.state.stockNews[4].url} 
+              />
+            </ul>
+          </div>
+        </div>
       </div>
+        
+
+      </div>
+
+
+      
       
       
     )
